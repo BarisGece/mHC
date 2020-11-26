0cd2799e636c88f85a60b8145c2ae059bc614ed5
Baris Gece <gece.baris@gmail.com>
2020-11-27 02:42:35 +0300

    terraform - variables updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
-#########################################################################
+#########################################################################################
 # Arguments are supported in the Terraform Telmate Proxmox 
 # ** Provider Block **
-#########################################################################
+#########################################################################################
 
 variable "api_url" {
   description = "This is the target Proxmox API endpoint. REQUIRED(or use environment variable PM_API_URL)"
@@ -47,11 +47,11 @@ variable "log_enable" {
 
 variable "log_levels" {
   description = "A map of log sources and levels"
-  type        = map(object({
-    _default          = string
-    _capturelog       = string
-    }))
-  default     = {
+  type = map(object({
+    _default    = string
+    _capturelog = string
+  }))
+  default = {
     _default    = "debug"
     _capturelog = ""
   }
@@ -69,10 +69,10 @@ variable "timeout" {
   default     = 600
 }
 
-#########################################################################
+#########################################################################################
 # Arguments are supported in the Terraform Telmate Proxmox 
 # ** Resource Block **
-#########################################################################
+#########################################################################################
 
 variable "name" {
   description = "Name of the VM. REQUIRED"
@@ -219,11 +219,11 @@ variable "scsihw" {
 }
 
 /*
- ** memory : Sets the VGA memory (in MiB). Has no effect with serial display. (4 - 512)
- ** type   : Set he VGA type. Default: std. (cirrus | none | qxl | qxl2 | qxl3 | qxl4 | serial0 | serial1 | serial2 | serial3 | std | virtio | vmware)
+ ** memory :                - Sets the VGA memory (in MiB). Has no effect with serial display. (4 - 512)
+ ** type   : Default: "std" - Set the VGA type (cirrus | none | qxl | qxl2 | qxl3 | qxl4 | serial0 | serial1 | serial2 | serial3 | std | virtio | vmware)
 */
 variable "vga" {
-  description = "Configure the VGA Hardware. Default(for type): std" 
+  description = "Configure the VGA Hardware. Default(for type): std"
   type = object({
     type   = string
     memory = number
@@ -232,28 +232,28 @@ variable "vga" {
 }
 
 /*
-  ** model     : Network Card Model. The virtio model provides the best performance with very low CPU overhead
-               : If your guest does not support this driver, it is usually best to use e1000
-               : (e1000 | e1000-82540em | e1000-82544gc | e1000-82545em | i82551 | i82557b | i82559er | ne2k_isa | ne2k_pci | pcnet | rtl8139 | virtio | vmxnet3)
-  ** macaddr   : A common MAC address with the I/G (Individual/Group) bit not set
-  ** bridge    : Bridge to attach the network device to. The Proxmox VE standard bridge is called vmbr0
-  ** tag       : VLAN tag to apply to packets on this interface
-  ** firewall  : Whether this interface should be protected by the firewall
-  ** rate      : Rate limit in mbps (megabytes per second) as floating point number
-  ** queues    : Number of packet queues to be used on the device
-  ** link_down : Whether this interface should be disconnected (like pulling the plug)
+  ** model     : REQUIRED       - Network Card Model. The virtio model provides the best performance with very low CPU overhead
+                                  If your guest does not support this driver, it is usually best to use e1000
+                                  (e1000 | e1000-82540em | e1000-82544gc | e1000-82545em | i82551 | i82557b | i82559er | ne2k_isa | ne2k_pci | pcnet | rtl8139 | virtio | vmxnet3)
+  ** macaddr   :                - A common MAC address with the I/G (Individual/Group) bit not set
+  ** bridge    : Default: "nat" - However; The Proxmox VE standard bridge is called vmbr0. Bridge to attach the network device to
+  ** tag       : Default: -1    - VLAN tag to apply to packets on this interface. (1 - 4094)
+  ** firewall  : Default: false - Whether this interface should be protected by the firewall
+  ** rate      :                - Rate limit in mbps (megabytes per second) as floating point number. (0 - N)
+  ** queues    :                - Number of packet queues to be used on the device. (0 - 16)
+  ** link_down :                - Whether this interface should be disconnected (like pulling the plug)
 */
 variable "network" {
   description = "Specify network devices"
   type = list(object({
-    model      = string
-    macaddr    = string
-    bridge     = string
-    tag        = number
-    firewall   = bool
-    rate       = number
-    queues     = number
-    link_down  = bool
+    model     = string
+    macaddr   = string
+    bridge    = string
+    tag       = number
+    firewall  = bool
+    rate      = number
+    queues    = number
+    link_down = bool
   }))
   default = [
     {
@@ -264,137 +264,241 @@ variable "network" {
       firewall  = false
       rate      = null
       queues    = null
-      link_down = null
+      link_down = false
     }
   ]
 }
 
+/*
+  ** type        : REQUIRED        - Disk Type - (ide|sata|scsi|virtio)
+  ** storage     : REQUIRED        - Target storage
+  ** size        : REQUIRED        - Disk size. This is purely informational and has no effect
+  ** format      :                 - Set the drive’s backing file’s data format (cloop | cow | qcow | qcow2 | qed | raw | vmdk)
+  ** cache       : Default: "none" - Set the drive’s cache mode (directsync | none | unsafe | writeback | writethrough)
+  ** backup      : Default: false  - Whether the drive should be included when making backups
+  ** iothread    : Default: false  - Whether to use iothreads for this drive
+  ** replicate   : Default: false  - Whether the drive should considered for replication jobs
+  ** ssd         :                 - Whether to expose this drive as an SSD, rather than a rotational hard disk
+  ** discard     :                 - Controls whether to pass discard/trim requests to the underlying storage
+  ** mbps        : Default: 0      - Maximum r/w speed in megabytes per second
+  ** mbps_rd     : Default: 0      - Maximum read speed in megabytes per second
+  ** mbps_rd_max : Default: 0      - Maximum unthrottled read pool in megabytes per second
+  ** mbps_wr     : Default: 0      - Maximum write speed in megabytes per second
+  ** mbps_wr_max : Default: 0      - Maximum unthrottled write pool in megabytes per second
+  ** file        :                 - The drive’s backing volume
+  ** media       :                 - Set the drive’s media type (cdrom | disk)
+  ** volume      :                 -
+  ** slot        :                 -
+*/
 variable "disk" {
-  description = "(optional) describe your variable"
+  description = "Specify disk variables"
   type = list(object({
-    type         = string
-    storage      = string
-    size         = number
-    format       = string
-    cache        = number
-    backup       = bool
-    iothread     = bool
-    replicate    = bool
-    ssd          = bool
-    file         = string
-    media        = string
-    discard      = string
-    mbps         = number
-    mbps_rd      = number
-    mbps_rd_max  = number
-    mbps_wr      = number
-    mbps_wr_max  = number
-    volume       = string
-    slog         = string
+    type        = string
+    storage     = string
+    size        = string
+    format      = string
+    cache       = string
+    backup      = bool
+    iothread    = bool
+    replicate   = bool
+    ssd         = bool
+    discard     = string
+    mbps        = number
+    mbps_rd     = number
+    mbps_rd_max = number
+    mbps_wr     = number
+    mbps_wr_max = number
+    file        = string
+    media       = string
+    volume      = string
+    slot        = number
   }))
-  default = []
+  default = [
+    {
+      type        = "scsi"
+      storage     = "local-lvm"
+      size        = "32G"
+      format      = "raw"
+      cache       = "none"
+      backup      = true
+      iothread    = true
+      replicate   = true
+      ssd         = null
+      discard     = "on"
+      mbps        = null
+      mbps_rd     = null
+      mbps_rd_max = null
+      mbps_wr     = null
+      mbps_wr_max = null
+      file        = null
+      media       = "disk"
+      volume      = null
+      slot        = null
+    }
+  ]
 }
 
+/*
+ ** id     : REQUIRED - ID is 0 to 3
+ ** type   : REQUIRED - socket
+*/
 variable "serial" {
-  description = "(optional)"
+  description = "Create a serial device inside the VM. Serial interface of type socket is used by xterm.js. Using a serial device as terminal"
   type = object({
-    id   = string
+    id   = number
     type = string
   })
-  default = null
+  default = {
+    id   = 0
+    type = "socket"
+  }
 }
 
 variable "pool" {
-  description = "Optional"
+  description = "The destination resource pool for the new VM"
   type        = string
   default     = null
 }
 
 variable "force_create" {
-  description = "Defaults to true"
+  description = "Default: false"
   type        = string
   default     = false
 }
 
 variable "clone_wait" {
-  description = "Optional"
-  type        = string
+  description = "Giving time(second) to Proxmox-VE to catchup. Default: 15"
+  type        = number
   default     = null
+}
+
+#########################################################################################
+# ** The following arguments are specifically for Linux for preprovisioning **
+# ** It phase which is used to set a hostname, intialize eth0, and resize the VM disk **
+# ** REQUIRES define_connection_info to be TRUE **
+#########################################################################################
+variable "define_connection_info" {
+  description = "Define the (SSH) connection parameters for preprovisioners. It allow user to opt-out of setting the connection info for the resource. Default: true"
+  type        = bool
+  default     = true
 }
 
 variable "preprovision" {
-  description = "Defaults to true"
-  type        = string
-  default     = null
+  description = "Enable/Disabled Pre-Provisioning. For more detail Telmate vm_qemu.md. Default: true"
+  type        = bool
+  default     = true
 }
 
 variable "os_type" {
-  description = "Which provisioning method to use, based on the OS type. Possible values: ubuntu, centos, cloud-init."
+  description = "Which provisioning method to use, based on the OS type. Possible values: ubuntu, centos, cloud-init. For more detail Telmate vm_qemu.md"
+  type        = string
+  default     = "cloud-init"
+}
+
+variable "os_network_config" {
+  description = "Linux provisioning specific, /etc/network/interfaces for Ubuntu and /etc/sysconfig/network-scripts/ifcfg-eth0 for CentOS"
   type        = string
   default     = null
 }
 
-#########################################################################
+variable "ssh_forward_ip" {
+  description = "Address used to connect to the VM"
+  type        = string
+  default     = null
+}
+
+variable "ssh_host" {
+  description = "Hostname or IP Address used to connect to the VM"
+  type        = string
+  default     = null
+}
+
+variable "ssh_port" {
+  description = "SSH port used to connect to the VM"
+  type        = string
+  default     = null
+}
+
+variable "ssh_user" {
+  description = "Username to login in the VM when preprovisioning"
+  type        = string
+  default     = null
+}
+
+variable "ssh_private_key" {
+  description = "Private key to login in the VM when preprovisioning"
+  type        = string
+  default     = null
+}
+
+#########################################################################################
 # Arguments are supported in the Terraform Telmate Proxmox 
-# ** Resource Block Cloud Init Specific Variables**
-#########################################################################
+# ** Resource Block Cloud Init Specific Variables **
+# ** Also the following arguments are specifically for Cloud-init for preprovisioning **
+#########################################################################################
 
 variable "ci_wait" {
-  description = "(optional)"
-  type        = string
+  description = "Cloud-init specific, how to long to wait for preprovisioning. Default: 30"
+  type        = number
   default     = null
 }
 
 variable "ciuser" {
-  description = "(optional)"
+  description = "Cloud-init specific, Overwrite image Default User"
   type        = string
   default     = null
 }
 
 variable "cipassword" {
-  description = "(optional)"
+  description = "Cloud-init specific, Password to assign the user. Using this is generally not recommended. Use ssh keys instead"
   type        = string
   default     = null
 }
 
 variable "cicustom" {
-  description = "(optional)"
+  description = "Cloud-init specific, location of the custom cloud-config files"
   type        = string
   default     = null
 }
 
 variable "searchdomain" {
-  description = "(optional)"
+  description = "Cloud-init specific, sets DNS search domains for a container"
   type        = string
   default     = null
 }
 
 variable "nameserver" {
-  description = "(optional)"
+  description = "Cloud-init specific, sets DNS server IP address for a container"
   type        = string
   default     = null
 }
 
 variable "sshkeys" {
-  description = "sshkeys"
+  description = "Setup public SSH keys (one key per line, OpenSSH format)"
   type        = string
   default     = null
 }
 
 variable "ipconfig0" {
-  description = "(optional)"
+  description = "Cloud-init specific, Specify IP addresses and gateways for the corresponding interface. [gw=] [,gw6=] [,ip=<IPv4Format/CIDR>] [,ip6=<IPv6Format/CIDR>]"
   type        = string
   default     = null
 }
 
 variable "ipconfig1" {
-  description = "(optional)"
+  description = "Cloud-init specific, Specify IP addresses and gateways for the corresponding interface. [gw=] [,gw6=] [,ip=<IPv4Format/CIDR>] [,ip6=<IPv6Format/CIDR>]"
   type        = string
   default     = null
 }
 
 variable "ipconfig2" {
-  description = "(optional)"
+  description = "Cloud-init specific, Specify IP addresses and gateways for the corresponding interface. [gw=] [,gw6=] [,ip=<IPv4Format/CIDR>] [,ip6=<IPv6Format/CIDR>]"
+  type        = string
+  default     = null
+}
+
+variable "force_recreate_on_change_of" {
+  description = "Allows this to depend on another resource, that when changed, needs to re-create this vm. An example where this is useful is a cloudinit configuration (as the cicustom attribute points to a file not the content)"
   type        = string
   default     = null
 }
